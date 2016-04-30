e995f1b85238b9a1f207fbc0552b6184eb5092c8
Jason Mitchell <jason@nullog.net>
2016-04-29 21:41:42 -0400

    tidy up/move all template elements into handelbars/

--- var/config.js
+++ var/config.js
@@ -1,7 +1,7 @@
 const blog_source = 'post/**/*.md' // will match zero or more directory in post
 
-import each_upto from '../lib/scripts/handlebars/each_upto'
-import ISODate from '../lib/scripts/handlebars/ISODate'
+import each_upto from '../handlebars/helpers/each_upto'
+import ISODate from '../handlebars/helpers/ISODate'
 
 const config = {
   destination: './build/_site',
@@ -9,8 +9,8 @@ const config = {
     engine: 'handlebars',
     pattern: '**/!(*.css|*.scss)+(*.md|*.html)',  // https://stackoverflow.com/questions/24222093/node-javascript-glob-file-path-matching-syntax-wildcards-etc
     default: 'default.hbs',
-    directory: 'layouts/',
-    partials: 'layouts/partials',
+    directory: 'handlebars/layouts/',
+    partials: 'handlebars/partials',
     helpers: {
       each_upto: each_upto,
       iso_date: ISODate
