02475c3c0189655a8a9c73f0ac89f61d7498ab67
Jason Mitchell <jason@nullog.net>
2017-06-08 16:55:17 -0400

    remove travis file and using new theme for site

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,7 @@
 import Typography from 'typography'
-import Wordpress2016 from 'typography-theme-wordpress-2016'
+import Github from 'typography-theme-github'
 
-const typography = new Typography(Wordpress2016)
+const typography = new Typography(Github)
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== 'production') {
