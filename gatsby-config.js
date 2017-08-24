c60bf24c5d8a8286a9a19fa0089ee3228c5291e9
Jason Mitchell <jason@nullog.net>
2017-08-23 21:54:43 -0400

    drop shap usage and gatsby-remark-images

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,12 +73,6 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 590
-            }
-          },
           'gatsby-remark-prismjs',
           'gatsby-remark-copy-linked-files',
           {
@@ -90,8 +84,8 @@ module.exports = {
         ]
       }
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+   // `gatsby-transformer-sharp`,
+   // `gatsby-plugin-sharp`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
   ]
