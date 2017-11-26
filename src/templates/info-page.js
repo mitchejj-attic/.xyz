c1599a3cb8a074769732e9467dfd3473366617f9
Jason Mitchell <jason@nullog.net>
2017-11-26 09:51:01 -0500

    proper titling for special pages

--- src/templates/info-page.js
+++ src/templates/info-page.js
@@ -9,9 +9,16 @@ import Link from 'gatsby-link'
 class InfoPageTemplate extends React.Component {
   render() {
     const post = this.props.data.markdownRemark
+    const postTitle = this.props.data.markdownRemark.frontmatter.title
     const siteTitle = this.props.data.site.siteMetadata.title
+
+    const helmet_title = post.frontmatter.title + ' | ' + siteTitle
+
     return (
+      
       <div className="blogContainer">
+      <Helmet title={`${helmet_title}`} />  
+        <div> {postTitle} | {siteTitle} </div>
         <div className="markdown" dangerouslySetInnerHTML={{ __html: post.html }} />
       </div>
     )
