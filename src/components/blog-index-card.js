c515999e263b0087b7c8e5c0e9e73e0489a5c0b7
Jason Mitchell <jason@nullog.net>
2018-04-25 11:46:35 -0400

    small fix, for now, for /all displaying nice on small screens and yarn up

--- src/components/blog-index-card.js
+++ src/components/blog-index-card.js
@@ -4,7 +4,6 @@ import graphql from 'graphql'
 import get from 'lodash/get'
 import Link from "gatsby-link"
 
-
 class BlogIndexCard extends React.Component {
     render() {
         const post = this.props.post
@@ -14,7 +13,6 @@ class BlogIndexCard extends React.Component {
                 <h2 className="index-title pl1 pr1 pt1 pb1"><Link to={post.frontmatter.path}>{post.frontmatter.title}</Link></h2>
                 <div className="index-markdown" dangerouslySetInnerHTML={{ __html: post.html }} />
         </div>
-
         )
     }
 }
